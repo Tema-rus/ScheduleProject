dd558ba88c81c34f747cf383d580fff289efdbcf
Артём Энгель <artemkot24@yandex.ru>
2025-10-30 22:28:42 +0700

    Add create_admin.py

---
+++ create_admin.py
@@ -0,0 +1,17 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kgau_schedule.settings')
+django.setup()
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'artemkot24@yandex.ru')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Суперпользователь {username} создан.')
+else:
+    print(f'Суперпользователь {username} уже существует.')
