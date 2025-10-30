a8d77dc5bd4ffd7b7613320d845f817e2cd8a759
Артём Энгель <artemkot24@yandex.ru>
2025-10-30 22:29:56 +0700

    Change create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -7,7 +7,7 @@ django.setup()
 
 User = get_user_model()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'artemkot24@yandex.ru')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 
 if not User.objects.filter(username=username).exists():
